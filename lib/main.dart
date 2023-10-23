d125e8366b152d785066b4731e745f2d0bfd709f
medoum <amacusnemo9596@gmail.com>
2023-10-23 23:52:43 +0100

    statelesswidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Text(
-    "Hello, world",
-    textDirection: TextDirection.ltr,
-  ));
+  runApp(MyApp());
+}
+
+// Types of Widgets
+// 1. StatelessWiget
+// 2. StateFullWidget
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext Context) {
+    return Text(
+      "Hello, world",
+      textDirection: TextDirection.rtl,
+    );
+  }
 }
